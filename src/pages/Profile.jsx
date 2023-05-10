f2570cd1cd4adfd88e99d01ffe6766aa04e4c239
MuhammadUmar969 <muhammadumarazizkhan23@gmail.com>
2023-05-11 01:41:13 +0500

    Listings

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { getAuth, updateProfile} from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify'
-import { doc, updateDoc } from 'firebase/firestore';
+import { collection, doc, getDocs, orderBy, query, updateDoc, where } from 'firebase/firestore';
 import {db} from '../firebase';
 import {FcHome} from 'react-icons/fc'
 import { Link } from 'react-router-dom';
+import ListingItems from '../components/ListingItems';
 
 const Profile = () => {
 
@@ -13,6 +14,10 @@ const Profile = () => {
   const navigate = useNavigate();
 
   const [changeDetails, setChangeDetails] = useState(false);
+
+  const [listings, setListings] = useState(null);
+
+  const [loading , setLoading] = useState(true);
 
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
@@ -54,6 +59,27 @@ const Profile = () => {
      }
   }
 
+  useEffect(() => {
+    async function fetchUserListings() {
+      const listingRef = collection(db, "listings");
+      const q = query(
+        listingRef,
+        where("userRef", "==", auth.currentUser.uid),
+        orderBy("timestamp", "desc")
+      );
+      const querySnap = await getDocs(q);
+      let listings = [];
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+      setListings(listings);
+      setLoading(false);
+    }
+    fetchUserListings();
+  }, [auth.currentUser.uid]);
   return (
     <>
       <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
@@ -106,6 +132,26 @@ const Profile = () => {
           </button>
         </div>
       </section>
+      <div className="max-w-6xl px-3 mt-6 mx-auto">
+        {!loading && listings.length > 0 && (
+          <>
+            <h2 className="text-2xl text-center font-semibold mb-6">
+            My Listings
+            </h2>
+
+            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4
+            2xl:grid-cols-5 mt-6 mb-6">
+            {listings.map((listing) => (
+                <ListingItems
+                  key={listing.id}
+                  id={listing.id}
+                  listing={listing.data}
+                />
+              ))}
+            </ul>
+          </>
+        )}
+      </div>
     </>
   )
 }
